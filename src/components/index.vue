3294b90a58208a7b7855fb6a60bfd2cef202819c
xiaoliao <xiaoliao1036@163.com>
2020-02-23 21:02:07 +0800

    add index

---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>这里是index</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: '这是index组件的内容'
+    }
+  }
+}
+</script>
+
+
